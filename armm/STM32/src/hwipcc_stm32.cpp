407d6563d02dad367a9d93b3b0edc0da2b9422ff
Bergi84 <Klaus.Bergkirchner@gmail.com>
2022-04-03 11:16:57 +0200

    ipcc implemented

--- armm/STM32/src/hwipcc_stm32.cpp
+++ armm/STM32/src/hwipcc_stm32.cpp
@@ -12,7 +12,98 @@
 
 #if defined(MCUSF_WB)
 
+bool THwIpcc_stm32::init()
+{
+  regs = IPCC;
 
+  // enable ipcc clock
+  RCC->AHB3ENR |= RCC_AHB3ENR_IPCCEN;
+
+  // enable interrupt generation
+  regs->C1CR |= IPCC_C1CR_RXOIE | IPCC_C1CR_TXFIE;
+
+  // enable interrupts
+  __NVIC_EnableIRQ(IPCC_C1_RX_IRQn);
+  __NVIC_EnableIRQ(IPCC_C1_TX_IRQn);
+
+  return true;
+}
+
+bool THwIpcc_stm32::startCPU2()
+{
+  RCC->C2AHB3ENR |= RCC_C2AHB3ENR_IPCCEN;
+
+  EXTI->C2EMR2 |= EXTI_C2IMR2_IM41;
+  EXTI->RTSR2 |= EXTI_RTSR2_RT41;
+
+  __SEV( );   // send event to all cpu's
+  __WFE( );   // clear event on this cpu
+
+  PWR->CR4 |= PWR_CR4_C2BOOT;
+
+  return true;
+}
+
+bool THwIpcc_stm32::enableRxChannel(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)())
+{
+  enableRxChannel(aChannel);
+  setRxCb(aChannel, mPObj, aPMFunc);
+  return true;
+}
+
+bool THwIpcc_stm32::enableRxChannel(channel_t aChannel, void (*aPFunc)())
+{
+  enableRxChannel(aChannel);
+  setRxCb(aChannel, aPFunc);
+  return true;
+}
+
+
+bool THwIpcc_stm32::setRxCb(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)())
+{
+  uint32_t index = 31 - __CLZ((int32_t) aChannel);
+  irqRxCbList[index].pObj = mPObj;
+  irqRxCbList[index].pMFunc = aPMFunc;
+  return true;
+}
+
+bool THwIpcc_stm32::setRxCb(channel_t aChannel, void (*aPFunc)())
+{
+  uint32_t index = 31 - __CLZ((int32_t) aChannel);
+  irqRxCbList[index].pObj = 0;
+  irqRxCbList[index].pFunc = aPFunc;
+  return true;
+}
+
+bool THwIpcc_stm32::enableTxChannel(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)())
+{
+  enableTxChannel(aChannel);
+  setTxCb(aChannel, mPObj, aPMFunc);
+  return true;
+}
+
+bool THwIpcc_stm32::enableTxChannel(channel_t aChannel, void (*aPFunc)())
+{
+  enableTxChannel(aChannel);
+  setTxCb(aChannel, aPFunc);
+  return true;
+}
+
+bool THwIpcc_stm32::setTxCb(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)())
+{
+  uint32_t index = 31 - __CLZ((int32_t) aChannel);
+  irqTxCbList[index].pObj = mPObj;
+  irqTxCbList[index].pMFunc = aPMFunc;
+  return true;
+}
+
+bool THwIpcc_stm32::setTxCb(channel_t aChannel, void (*aPFunc)())
+{
+  uint32_t index = 31 - __CLZ((int32_t) aChannel);
+  irqTxCbList[index].pObj = 0;
+  irqTxCbList[index].pFunc = aPFunc;
+  return true;
+}
 
 #endif // defined(MCUSF_WB)
 
--- armm/STM32/src/hwipcc_stm32.h
+++ armm/STM32/src/hwipcc_stm32.h
@@ -13,7 +13,146 @@
 
 class THwIpcc_stm32 : public THwIpcc_pre
 {
+public:
+  IPCC_TypeDef* regs;
 
+  typedef struct {
+    TCbClass* pObj;
+    union {
+      void (TCbClass::*pMFunc)();
+      void (*pFunc)();
+    };
+  } cbRec_t;
+
+  cbRec_t irqRxCbList[6];
+  cbRec_t irqTxCbList[6];
+
+  typedef enum {
+    CH_1 = 1,
+    CH_2 = 2,
+    CH_3 = 4,
+    CH_4 = 8,
+    CH_5 = 16,
+    CH_6 = 32,
+  }channel_t;
+
+  // enable clock and irq for ipcc
+  bool init();
+
+  bool startCPU2();
+
+  bool enableRxChannel(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)());
+  bool enableRxChannel(channel_t aChannel, void (*aPFunc)());
+  inline bool enableRxChannel(channel_t aChannel)
+  {
+   regs->C1MR &= ~(((int32_t) aChannel) << IPCC_C1MR_CH1OM_Pos);
+   return true;
+  }
+
+  bool setRxCb(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)());
+  bool setRxCb(channel_t aChannel, void (*aPFunc)());
+
+  inline bool disableRxChannel(channel_t aChannel)
+  {
+    regs->C1MR |= ((int32_t) aChannel) << IPCC_C1MR_CH1OM_Pos;
+    return true;
+  }
+
+  bool enableTxChannel(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)());
+  bool enableTxChannel(channel_t aChannel, void (*aPFunc)());
+  inline bool enableTxChannel(channel_t aChannel)
+  {
+    regs->C1MR &= ~(((int32_t) aChannel) << IPCC_C1MR_CH1FM_Pos);
+    return true;
+  }
+  bool setTxCb(channel_t aChannel, TCbClass* mPObj, void (TCbClass::*aPMFunc)());
+  bool setTxCb(channel_t aChannel, void (*aPFunc)());
+
+  inline bool diableTxChannel(channel_t aChannel)
+  {
+    regs->C1MR |= ((int32_t) aChannel) << IPCC_C1MR_CH1FM_Pos;
+    return true;
+  }
+
+  // set transmit channel flag
+  inline bool setTxFlag(channel_t aChannel)
+  {
+    regs->C1SCR = ((int32_t) aChannel) << IPCC_C1SCR_CH1S_Pos;
+    return true;
+  }
+
+  // clears receive channel flag
+  inline bool clearRxFlag(channel_t aChannel)
+  {
+    regs->C1SCR = ((int32_t) aChannel) << IPCC_C1SCR_CH1C_Pos;
+    return true;
+  }
+
+  // get transmit occupied channel flag
+  inline bool getTxActivFlag(channel_t aChannel)
+  {
+    return (regs->C1TOC2SR & (uint32_t) aChannel) == (uint32_t) aChannel;
+  }
+
+  inline bool getTxPending(uint32_t &channels)
+  {
+    channels = regs->C1TOC2SR & ((~regs->C1MR) >> IPCC_C1MR_CH1FM_Pos);
+    return true;
+  }
+
+  // get receive pending channel flag
+  inline bool getRxActivFlag(channel_t aChannel)
+  {
+    return (regs->C2TOC1SR & (uint32_t) aChannel) == (uint32_t) aChannel;
+  }
+
+  inline bool getRxPending(uint32_t &channels)
+  {
+    channels = regs->C2TOC1SR & ((~regs->C1MR) >> IPCC_C1MR_CH1OM_Pos);
+    return true;
+  }
+
+  inline void rxIrqHandler()
+  {
+    uint32_t rxPendCh;
+    getRxPending(rxPendCh);
+    for(uint32_t i = (uint32_t)CH_1; i <= (uint32_t)CH_6; i << 1)
+    {
+      if(rxPendCh & i)
+      {
+        cbRec_t* cb = &irqRxCbList[31 - __CLZ((int32_t) i)];
+        if(cb->pObj)
+        {
+          ((cb->pObj)->*(cb->pMFunc))();
+        }
+        else
+        {
+          cb->pFunc();
+        }
+      }
+    }
+  }
+
+  inline void txIrqHandler()
+  {
+    uint32_t txPendCh;
+    getTxPending(txPendCh);
+    for(uint32_t i = (uint32_t)CH_1; i <= (uint32_t)CH_6; i << 1)
+    {
+      if(txPendCh & i)
+      {
+        cbRec_t* cb = &irqTxCbList[31 - __CLZ((int32_t) i)];
+        if(cb->pObj)
+        {
+          ((cb->pObj)->*(cb->pMFunc))();
+        }
+        else
+        {
+          cb->pFunc();
+        }
+      }
+    }
+  }
 };
 
 #define HWIPCC_IMPL THwIpcc_stm32
